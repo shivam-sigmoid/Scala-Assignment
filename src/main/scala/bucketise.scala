4e3496c51198c4e85bb63fdefbdf5307870c814f
Shivam Raj <shivamraj@AS-MAC-0208.local>
2022-02-13 20:22:58 +0530

    Added Comments

--- src/main/scala/bucketise.scala
+++ src/main/scala/bucketise.scala
@@ -1,10 +1,12 @@
 object bucketise {
   def truncateAt(x: Double, p: Int): Double = {
+    // To Truncate the value
     val s = math.pow(10, p)
     (x * s).toInt / s
   }
 
   def bucket(num: Double): (Double, String, Double) = {
+    // Used Mathematics to find the bucket in the range
     var initialValue: Double = (num / 0.05).floor * 0.05;
     var finalValue: Double = (num / 0.05).floor * 0.05 + 0.049;
     return (truncateAt(initialValue, 3),"-", truncateAt(finalValue, 3))
